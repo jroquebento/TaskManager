71f9cf2f535874ec0a6f09145361d38374041671
Júlio César <47599243+jroquebento@users.noreply.github.com>
2026-08-17 18:00:44 -0300

    feat: configure Entity Framework Core persistence (#4)

---
+++ src/TaskManager.Infrastructure/Data/TaskManagerDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TaskManager.Infrastructure.Data;
+
+internal class TaskManagerDbContext : DbContext
+{
+    public TaskManagerDbContext(DbContextOptions<TaskManagerDbContext> options) : base(options)
+    {
+        
+    }
+}
