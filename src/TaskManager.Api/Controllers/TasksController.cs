74f27d96d5b98e4f8efa2717f3ba0d5c0426d27e
Júlio César <47599243+jroquebento@users.noreply.github.com>
2026-08-19 10:58:14 -0300

    feat: implement create task endpoint (#14)

---
+++ src/TaskManager.Api/Controllers/TasksController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManager.Application.DTOs;
+using TaskManager.Application.UseCases.CreateTask;
+
+namespace TaskManager.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TasksController : ControllerBase
+{
+    private readonly CreateTaskUseCase _createTaskUseCase;
+
+    public TasksController(CreateTaskUseCase createTaskUseCase)
+    {
+        _createTaskUseCase = createTaskUseCase;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateTaskRequest request)
+    {
+        var taskItem = await _createTaskUseCase.Execute(request);
+
+        return Created(string.Empty, taskItem);
+    }
+}
