20911d324eae327bc050fa7120b94c243bf90fb5
Júlio César <47599243+jroquebento@users.noreply.github.com>
2026-08-21 11:51:10 -0300

    feat: implement task status transitions (#26)

---
+++ src/TaskManager.Application/UseCases/StartTask/StartTaskUseCase.cs
@@ -0,0 +1,25 @@
+﻿using TaskManager.Application.Interfaces;
+
+namespace TaskManager.Application.UseCases.StartTask;
+
+public class StartTaskUseCase
+{
+    private readonly ITaskRepository _taskRepository;
+    public StartTaskUseCase(ITaskRepository taskRepository)
+    {
+        _taskRepository = taskRepository;
+    }
+
+    public async Task<bool> ExecuteAsync(Guid id) 
+    {
+        var taskItem = await _taskRepository.GetByIdAsync(id);
+        if (taskItem == null)
+        {
+            return false;
+        }
+        taskItem.Start();
+        await _taskRepository.UpdateAsync(taskItem);
+
+        return true;
+    }
+}
